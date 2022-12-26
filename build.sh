3fd0efb0e07f8805fc9680805f1eaa40ecfbfd19
segfaultdev <segfaultdev@protonmail.com>
2022-12-26 01:07:38 +0100

    Cleaned up "build.sh".

--- build.sh
+++ build.sh
@@ -4,10 +4,7 @@ CFLAGS="-Iinclude -Os -s -Wall -Wextra"
 LDFLAGS="-s"
 
 FINDFLAGS=
-
-if [ -f ./bedd ]; then
-  FINDFLAGS="-newer ./bedd"
-fi
+if [ -f ./bedd ]; then FINDFLAGS="-newer ./bedd"; fi
 
 find -name "*.c" $FINDFLAGS | xargs -I{} sh -c "echo {}; gcc {} $CFLAGS -c"
 gcc $(find . -name "*.o") $LDFLAGS -o bedd
